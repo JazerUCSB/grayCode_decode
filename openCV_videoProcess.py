20f50e15d0bc4f54d4261225167a018950d53f96
Jazer Sibley-Schwartz <141766786+JazerUCSB@users.noreply.github.com>
2024-05-06 17:20:58 -0700

    stillnovideo

--- openCV_videoProcess.py
+++ openCV_videoProcess.py
@@ -47,7 +47,6 @@ def main(video_path, start_frame=0, end_frame=None, zoom_factor=1.0, rotation_an
         cap.release()
         return
 
-    # Initialize previous frame for EMA
     prev_frame = None
     prev_frame2 = None
 
@@ -69,34 +68,27 @@ def main(video_path, start_frame=0, end_frame=None, zoom_factor=1.0, rotation_an
         ret, frame = cap.read()
 
         # Check if the frame was read successfully
-        #if not ret:
+        if not ret:
             # If we reached the end of the video, loop back to the start frame
-            #cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-            #continue
+            cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+            continue
         
-        # Apply zoom, rotation, and translation transformations to the frame
-        # Zooming
         frame = cv2.resize(frame, None, fx=zoom_factor, fy=zoom_factor)
          # Calculate the mean value of the pixels in the frame
         blurred_frame = cv2.medianBlur(frame, 5)
         mean_value = np.mean(blurred_frame)
 
-        # Use the mean value for further processing or operations
-        # For example, you can use it as a threshold value in a thresholding operation
         _, frame = cv2.threshold(blurred_frame, mean_value, 255, cv2.THRESH_BINARY)
 
-        # Rotation
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         center = (width // 2, height // 2)
         rotation_matrix = cv2.getRotationMatrix2D(center, rotation_angle, 1.0)
         frame = cv2.warpAffine(frame, rotation_matrix, (width, height))
 
-        # Translation
         translation_matrix = np.float32([[1, 0, translation_x], [0, 1, translation_y]])
         frame = cv2.warpAffine(frame, translation_matrix, (width, height))
 
-        # Apply exponential moving average (EMA) for temporal smoothing
         if prev_frame is None:
             smoothed_frame = frame
         else:
@@ -108,36 +100,21 @@ def main(video_path, start_frame=0, end_frame=None, zoom_factor=1.0, rotation_an
             second_smooth = cv2.addWeighted(frame, .1, prev_frame2, .99, 0)
 
 
-        # Store the current frame for the next iteration
         prev_frame = smoothed_frame.copy()
         prev_frame2 = second_smooth.copy()
 
-        # Apply median filter
         blurred_frame = cv2.medianBlur(smoothed_frame, 5)
         blurred_frame2 = cv2.medianBlur(second_smooth, 5)
 
-        # Apply mean filter along y-direction
-        #kernel_size_y = (3, 3)  # Adjust the kernel size as needed
-        #filtered_frame = cv2.blur(blurred_frame, kernel_size_y)
-
-        kernelY = np.array([[0, 1, 0],
-                       [1, 1, 1],
-                       [0, 1, 0]], dtype=np.float32) / 5
-        
-        kernelX = np.array([[0, 1, 0],
-                       [0, 1, 0],
-                       [0, 1, 0]], dtype=np.float32) / 3
+       
         
         kernel = np.ones((5,5))        
-
-        # Apply the custom kernel using filter2D
-        #filtered_frame = cv2.filter2D(blurred_frame, -1, kernelX)
         erode = cv2.erode(blurred_frame, kernel, iterations = 1)
         erode2 = cv2.erode(blurred_frame2, kernel, iterations = 1)
 
         dilate = cv2.dilate(erode, kernel, iterations = 1)
         dilate2 = cv2.dilate(erode2, kernel, iterations = 1)
-        #avg = cv2.filter2D(filtered_frame, -1, kernel)
+    
 
         
 
@@ -157,33 +134,25 @@ def main(video_path, start_frame=0, end_frame=None, zoom_factor=1.0, rotation_an
 
         threshold =  (masked_frame > 240).astype(np.uint8) * 255
         threshold2 =  (masked_frame2 > 240).astype(np.uint8) * 255
-        # Convert the image to grayscale
+        
         gray = cv2.cvtColor(threshold, cv2.COLOR_BGR2GRAY)
         gray2 = cv2.cvtColor(threshold2, cv2.COLOR_BGR2GRAY)
         
         _, thresh = cv2.threshold(gray, 127, 255, 0)
         _, thresh2 = cv2.threshold(gray2, 127, 255, 0)
 
-        #erode = cv2.erode(thresh, kernel, iterations = 1)
+       
         dilate = cv2.dilate(thresh, kernel, iterations = 2)
         dilate2 = cv2.dilate(thresh2, kernel, iterations = 1)
         dilate2 = cv2.dilate(dilate2, kernel, iterations = 1)
-        #__, contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # Perform corner detection using Shi-Tomasi corner detection
-        corners = cv2.goodFeaturesToTrack(gray2, maxCorners=100, qualityLevel=0.01, minDistance=5)
-
-        # Convert corners to integers
-        corners = np.intp(corners)
-
        
-        
-        
+        corners = cv2.goodFeaturesToTrack(gray2, maxCorners=100, qualityLevel=0.01, minDistance=5)
+        corners = np.intp(corners)
 
         maxi = np.maximum(dilate, dilate2)
         miny = 10000
         maxy = 0
         
-        # Draw detected corners on the frame
         for corner in corners:
             x, y = corner.ravel()
             if y > maxy:
@@ -223,7 +192,6 @@ def main(video_path, start_frame=0, end_frame=None, zoom_factor=1.0, rotation_an
             else:
                 binary_rep += '1' if binary_rep[i - 1] == '0' else '0'
 
-
         for i, bit in enumerate(gray_rep):
             cv2.putText(maxi, bit, (x_pos, y_pos + i * line_height), font_face, font_scale, (255, 255, 255), font_thickness, cv2.LINE_AA)
 
@@ -234,7 +202,6 @@ def main(video_path, start_frame=0, end_frame=None, zoom_factor=1.0, rotation_an
         numString = str(number)
 
         cv2.putText(maxi, numString, (x_pos +100, y_pos + i * line_height), font_face, font_scale, (255, 255, 255), font_thickness, cv2.LINE_AA)    
-
 
         rr, cc = draw.line(250, miny, 750, miny)
         rr2, cc2 = draw.line(250, maxy, 750, maxy)
@@ -251,23 +218,19 @@ def main(video_path, start_frame=0, end_frame=None, zoom_factor=1.0, rotation_an
         lnMax = np.maximum(ln, np.maximum(ln2, ln3))
         maxi = np.maximum(maxi, lnMax)
 
-        out.write(maxi)  # Write the frame to the output video
+        #out.write(maxi.astype(np.uint8))  # Write the frame to the output video
 
-        # Display the masked frame
-        #cv2.imshow("Video", maxi)   
-        if frame_number >= end_frame:
-            break
-
-        
-
-        # Wait for 25 milliseconds, and check for key press 'q' to exit the loop
-        #if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.imshow("Video", maxi)   
+        #if frame_number >= end_frame:
             #break
 
-        # Check if we reached the end frame
-        #if cap.get(cv2.CAP_PROP_POS_FRAMES) >= end_frame:
-            # If we reached the end frame, loop back to the start frame
-            #cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+        # Wait for 25 milliseconds, and check for key press 'q' to exit the loop
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) >= end_frame:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
     # Release the VideoWriter object
     out.release()
@@ -281,7 +244,6 @@ if __name__ == "__main__":
         print("Usage: python script_name.py video_file_path start_frame end_frame zoom_factor rotation_angle translation_x translation_y")
         sys.exit(1)
 
-    # Get the video file path, start frame, end frame, zoom factor, rotation angle, translation x, and translation y from the command-line arguments
     video_path = sys.argv[1]
     start_frame = int(sys.argv[2])
     end_frame = int(sys.argv[3])
